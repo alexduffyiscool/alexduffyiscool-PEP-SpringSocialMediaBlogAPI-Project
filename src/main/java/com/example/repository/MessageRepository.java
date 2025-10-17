169e77b779176729bc9a08be35349a7cf7817f83
alexduffyiscool <alexduffyiscool@github.com>
2025-10-17 23:21:03 +0000

    act msg repo

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.entity.Account;
+import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Account, Integer> {
