ae7f4ee0a66a15e6175097bc90e8b93864891085
alexduffyiscool <alexduffyiscool@github.com>
2025-10-17 23:29:58 +0000

    act service

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,8 +1,11 @@
 package com.example.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.entity.Account;
+import java.util.Optional;
 
 @Respository
 public interface AccountRepository {
   Optional<Account> findByUsername(String username);
   Optional<Account> findByUsernameAndPassword(String username, String password);
-
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,28 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import java.util.Optional;
+
+
+@Service
 public class AccountService {
+  @Autowired
+  private AccountRepository accountRepository;
+
+  public Optional<Account> register(Account account)
+  {
+    if (account.getUsername() == null || account.getUsername().isBlank() || account.getUsername().length() < 4)
+    {
+      return Optional.empty();
+    }
+    return Optional.of(accountRepository.save(account));
+  }
+
+  public Optional<Account> login(String username, String password)
+  {
+    return accountRepository.findByUsernameAndPassword(username, password);
+  }
 }
